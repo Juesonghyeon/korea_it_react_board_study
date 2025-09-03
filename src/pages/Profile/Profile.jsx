fab88d3174439b04b16340f5d131e041747c89ce
Juesonghyeon <199971alfks@gmail.com>
2025-09-03 21:49:59 +0900

    Feat:

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,85 +6,110 @@ import ChangePassword from "../../components/ChangePassword/ChangePassword";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { usePrincipalState } from "../../store/usePrincipalStore";
-import { sendMailRequest } from "../../apis/account/accountapis";
+import { sendMailRequest } from "../../apis/account/accountApis";
+import ChangeProfileImg from "../../components/ChangeProfileImg/ChangeProfileImg";
 
 function Profile() {
-  const [tab, setTab] = useState("myboard");
-  const [tabChild, setTabChild] = useState(1);
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const [searchParams] = useSearchParams();
-  const { isLoggedIn, principal } = usePrincipalState();
+	const [tab, setTab] = useState("myboard");
+	const [tabChild, setTabChild] = useState(1);
+	const navigate = useNavigate();
+	const { pathname } = useLocation();
+	const [searchParams] = useSearchParams();
+	const { isLoggedIn, principal } = usePrincipalState();
 
-  const tabClickHandler = (path) => {
-    setTabChild(path === "myboard" ? 1 : 2);
-    navigate(`${pathname}?tab=${path}`);
-  };
+	const tabClickHandler = (path) => {
+		setTabChild(path === "myboard" ? 1 : path === "changepassword" ? 2 : 3);
+		navigate(`${pathname}?tab=${path}`);
+	};
 
 	const onClickVerifyHandler = () => {
 		sendMailRequest({
-			email: principal.email
+			email: principal.email,
 		}).then((response) => {
 			if (response.data.status === "success") {
 				alert(response.data.message);
-			} else if (response.data.status === "failed"){
+			} else if (response.data.status === "failed") {
 				alert(response.data.message);
 			}
 		});
 	};
 
-  useEffect(() => {
-    setTab(searchParams.get("tab"));
-    setTabChild(
-      searchParams.get("tab") === "myboard" || searchParams.get("tab") === null
-        ? 1
-        : 2
-    );
-  }, [pathname, searchParams]);
+	useEffect(() => {
+		setTab(searchParams.get("tab"));
+		setTabChild(
+			searchParams.get("tab") === "myboard" ||
+				searchParams.get("tab") === null
+				? 1
+				: searchParams.get("tab") === "changepassword"
+				? 2
+				: 3
+		);
+	}, [pathname, searchParams]);
 
-  return (
-    <div css={s.container}>
-      <div css={s.profileContainer}>
-        <div css={s.profileHeader}>
-          <div css={s.profileImgBox}>
-            <div>
-              <img src={profileImg} alt="profileImage" />
-            </div>
-          </div>
-          <div css={s.profileInfoBox}>
-            <h3>{principal?.username}</h3>
-            <div>
-              <p>{principal?.email}</p>
-              {principal?.authorities[0].authority === "ROLE_TEMPORARY" ? (
-                <button onClick={onClickVerifyHandler}>
+	return (
+		<div css={s.container}>
+			<div css={s.profileContainer}>
+				<div css={s.profileHeader}>
+					<div css={s.profileImgBox}>
+						<div>
+							<img
+								src={principal?.profileImg}
+								alt="profileImage"
+							/>
+						</div>
+					</div>
+					<div css={s.profileInfoBox}>
+						<h3>{principal?.username}</h3>
+						<div>
+							<p>{principal?.email}</p>
+							{principal?.authorities[0].authority ===
+							"ROLE_TEMPORARY" ? (
+								<button onClick={onClickVerifyHandler}>
 									인증하기
 								</button>
-              ) : (
-                <></>
-              )}
-            </div>
-          </div>
-        </div>
-        <div css={s.profileBox}>
-          <div css={s.profileTab(tabChild)}>
-            <ul>
-              <li onClick={() => tabClickHandler("myboard")}>내 게시물</li>
-              <li onClick={() => tabClickHandler("changepassword")}>
-                비밀번호 변경
-              </li>
-            </ul>
-          </div>
-          <div css={s.profileMain}>
-            {tab === "myboard" || tab === null ? (
-              <MyBoard userId={principal?.userId} />
-            ) : (
-              <ChangePassword />
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+							) : (
+								<></>
+							)}
+						</div>
+					</div>
+				</div>
+				<div css={s.profileBox}>
+					<div css={s.profileTab(tabChild)}>
+						<ul>
+							<li onClick={() => tabClickHandler("myboard")}>
+								내 게시물
+							</li>
+							<li
+								onClick={() =>
+									tabClickHandler("changepassword")
+								}
+							>
+								비밀번호 변경
+							</li>
+							<li
+								onClick={() =>
+									tabClickHandler("changeprofileimg")
+								}
+							>
+								프로필 이미지 변경
+							</li>
+						</ul>
+					</div>
+					<div css={s.profileMain}>
+						{tab === "myboard" || tab === null ? (
+							<MyBoard userId={principal?.userId} />
+						) : tab === "changepassword" ? (
+							<ChangePassword />
+						) : (
+							<ChangeProfileImg
+								oldProfileImg={principal.profileImg}
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Profile;
